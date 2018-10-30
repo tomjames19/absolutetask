7cbea1307b89614a9c7012639af7bfb171f4bb22
Thomas Christensen <christensenthomas19@gmail.com>
2018-10-30 15:24:08 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  cognito: {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    USER_POOL_ID: "us-east-1_S9kjivoFW",
+    APP_CLIENT_ID: "7rnle8t3moqsuodf71mfku38mn",
+    REGION: "us-east-1",
+    IDENTITY_POOL_ID: "us-east-1:bd60b3ea-04c4-4795-bf21-d1dd0e504d46"
+  },
+  apiGateway: {
+    URL: "https://m8uekpwgnl.execute-api.us-west-1.amazonaws.com/prod",
+    REGION: "us-west-1"
+  },
+  s3: {
+  BUCKET: "note-app-tutorial-tjc"
+  }
+};
